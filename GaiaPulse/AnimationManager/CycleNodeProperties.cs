29ec89775d47d1e47d0fd9077ec07e44bf73e151
Justin Hughart <justinhughart@gmail.com>
2013-09-15 02:04:41 -0500

    -Removed the tags list.

--- GaiaPulse/AnimationManager/CycleNodeProperties.cs
+++ GaiaPulse/AnimationManager/CycleNodeProperties.cs
@@ -15,8 +15,6 @@ namespace GaiaPulse.AnimationManager
     {
         private AnimNode _node;
 
-        Dictionary<String, String> _dict;
-
         public CycleNodeProperties(AnimNode node, String id)
         {
             InitializeComponent();
@@ -26,61 +24,6 @@ namespace GaiaPulse.AnimationManager
             _node = node;
 
             LoadData();
-        }
-
-        private void LoadTags()
-        {
-            _dict = _node.GetTags();
-
-            foreach (var item in _dict)
-            {
-                object[] str = new object[2];
-                str[0] = item.Key;
-                str[1] = item.Value;
-
-                var row = new DataGridViewRow();
-                row.CreateCells(dgvTags);
-                row.SetValues(str);
-
-                dgvTags.Rows.Add(row);
-            }
-        }
-
-        private void SaveTags()
-        {
-            _dict.Clear();
-
-            foreach (DataGridViewRow row in dgvTags.Rows)
-            {
-                DataGridViewCell keycell = row.Cells[0];
-                DataGridViewCell valuecell = row.Cells[1];
-
-                String key = "";
-                String value = "";
-
-                if (keycell != null)
-                {
-                    if (keycell.Value != null)
-                    {
-                        key = keycell.Value.ToString();
-                    }
-                }
-
-                if (valuecell != null)
-                {
-                    if (valuecell.Value != null)
-                    {
-                        value = valuecell.Value.ToString();
-                    }
-                }
-
-                if (key != "")
-                {
-                    _dict.Add(key, value);
-                }
-            }
-
-            _node.SetTags(_dict);
         }
 
         private void LoadData()
@@ -94,8 +37,6 @@ namespace GaiaPulse.AnimationManager
             chkSmoothRotation.Checked = _node.SmoothRotation;
 
             txtTimeTillNext.Text = _node.TimeTillNext.ToString();
-
-            LoadTags();
         }
 
         private void BtnSaveClick(object sender, EventArgs e)
@@ -103,8 +44,6 @@ namespace GaiaPulse.AnimationManager
             _node.SetVelocity(new Vector2(float.Parse(txtVelocityX.Text), float.Parse(txtVelocityY.Text)), chkSmoothX.Checked, chkSmoothY.Checked);
             _node.SetRotation(float.Parse(txtRotation.Text), chkSmoothRotation.Checked);
             _node.SetTTN(int.Parse(txtTimeTillNext.Text));
-
-            SaveTags();
 
             Close();
         }
