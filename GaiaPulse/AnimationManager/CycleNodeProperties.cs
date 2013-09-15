02849dd9c72f9b7cc7f0088deb36fd2407c47a16
Justin Hughart <justinhughart@gmail.com>
2013-09-15 03:08:13 -0500

    -Added loading of custom XML to the tree view.

--- GaiaPulse/AnimationManager/CycleNodeProperties.cs
+++ GaiaPulse/AnimationManager/CycleNodeProperties.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml.Linq;
 using GaiaPulse.SC.FrameAnimation;
 using Microsoft.Xna.Framework;
 
@@ -14,7 +15,7 @@ namespace GaiaPulse.AnimationManager
     public partial class CycleNodeProperties : Form
     {
         private AnimNode _node;
-
+        
         public CycleNodeProperties(AnimNode node, String id)
         {
             InitializeComponent();
@@ -37,6 +38,8 @@ namespace GaiaPulse.AnimationManager
             chkSmoothRotation.Checked = _node.SmoothRotation;
 
             txtTimeTillNext.Text = _node.TimeTillNext.ToString();
+
+            LoadXML();
         }
 
         private void BtnSaveClick(object sender, EventArgs e)
@@ -45,7 +48,60 @@ namespace GaiaPulse.AnimationManager
             _node.SetRotation(float.Parse(txtRotation.Text), chkSmoothRotation.Checked);
             _node.SetTTN(int.Parse(txtTimeTillNext.Text));
 
+            SaveXML();
+
             Close();
+        }
+
+        private void LoadXML()
+        {
+            var root = _node.CustomXML;
+
+            treeXML.Nodes.Clear();
+            
+            RecurseLoad(root, null);
+        }
+
+        private void RecurseLoad(XElement element, TreeNode parent)
+        {
+            TreeNode node = new TreeNode(element.Name.ToString());
+            TreeNode attributenode = new TreeNode("Attributes");
+            node.Nodes.Add(attributenode);
+
+            var attributes = element.Attributes();
+
+            foreach (var attribute in attributes)
+            {
+                TreeNode newnode = new TreeNode(attribute.Name + " = " + attribute.Value);
+                attributenode.Nodes.Add(newnode);
+            }
+
+            if (parent == null)
+            {
+                treeXML.Nodes.Add(node);
+            }
+            else
+            {
+                parent.Nodes.Add(node);
+            }
+
+            var elementsnode = new TreeNode("Elements");
+            node.Nodes.Add(elementsnode);
+
+            var children = element.Elements();
+
+            if (children.Count() > 0)
+            {
+                foreach (var child in children)
+                {
+                    RecurseLoad(child, elementsnode);
+                }
+            }
+        }
+
+        private void SaveXML()
+        {
+            
         }
     }
 }
