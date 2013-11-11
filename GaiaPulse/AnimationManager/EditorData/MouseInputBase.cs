0822198cb5c1883cc5bb4392ccc2b9a4e58a5971
Justin Hughart <justinhughart@gmail.com>
2013-11-11 02:54:53 -0800

    -Added a base for all mouse input.

---
+++ GaiaPulse/AnimationManager/EditorData/MouseInputBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GaiaPulse.AnimationManager.EditorData
+{
+    public class MouseInputBase
+    {
+        protected EditorControl Editor;
+
+        public bool IsGrabbing { get; private set; }
+        public string GrabTarget { get; private set; }
+
+        public MouseInputBase(EditorControl editor)
+        {
+            Editor = editor;
+        }
+
+        public virtual bool Update() //Returns whether or not it's in control. This is for camera control.
+        {
+            return false;
+        }
+
+        public void Grab(String target)
+        {
+            IsGrabbing = true;
+            GrabTarget = target;
+        }
+
+        public void Release()
+        {
+            IsGrabbing = false;
+            GrabTarget = "";
+        }
+    }
+}
